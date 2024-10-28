8357028e8a75d7aeebcdb5917b584627c88dac8a
Rafał Więcek <gitzzz14@gmail.com>
2024-10-28 10:23:13 +0100

    Optimized loop performance in serializer

--- src/sjs.ts
+++ src/sjs.ts
@@ -15,14 +15,17 @@ const sjs = (schema: SjsSchema): SjsSerializer => {
     // during schema preparation => e.g. array stringification method.
     const queue = _makeQueue(preparedSchema, schema);
     const chunks = _makeChunks(preparedString, queue);
+    
     const chunkLength = chunks.length - 1;
+    const queueLength = queue.length;
+
     const selectChunk = _select(chunks);
 
     // Exposed function
     return (obj) => {
         let temp = '';
 
-        for (let i = 0; i < queue.length; ++i) {
+        for (let i = 0; i < queueLength; ++i) {
             const { serializer, find } = queue[i]!;
             const raw = find(obj);
 
